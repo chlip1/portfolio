fbf45eae518cdb1da98a4dd471cea60486bee8a4
Łukasz Chlipała <lukasz.chlipala3@gmail.com>
2025-01-21 15:49:24 +0100

    feat: increase page visibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,27 +7,35 @@ import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 export default function RootLayout({
- children,
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pl">
     <head>
-      <title>Łukasz Chlipała - Portfolio</title>
-      <meta name="description" content="My amazing Next.js application"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <link rel="icon" href="/favicon.ico"/>
+      <meta charSet="UTF-8" />
+      <meta
+        name="description"
+        content="Portfolio Łukasza Chlipały – Fullstack Developera specjalizującego się w chmurze oraz nowoczesnych technologiach webowych, takich jak React, Next.js i AWS."
+      />
+      <meta
+        name="keywords"
+        content="Łukasz Chlipała, fullstack developer, portfolio, cloud, AWS, React, Next.js, TypeScript, web development, programista chmura"
+      />
+      <meta name="author" content="Łukasz Chlipała" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="robots" content="index, follow" />
+
+      <link rel="icon" href="/favicon.ico" />
+
+      <title>Łukasz Chlipała - Portfolio Fullstack Developera</title>
     </head>
-    <body className="bg-white text-gray-900 dark:text-gray-100">
-    <Toaster
-      position="bottom-right"
-    />
-    <Navbar/>
-    <AOSProvider>
-      {children}
-    </AOSProvider>
-    <Footer/>
+    <body className="bg-white text-gray-900">
+    <Toaster position="bottom-right" />
+    <Navbar />
+    <AOSProvider>{children}</AOSProvider>
+    <Footer />
     </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,34 @@ import EducationSection from "@/components/EducationSection";
 import TechnologiesSection from "@/components/TechnologiesSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import ContactSection from "@/components/ContactSection";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main>
-      <HeroSection />
-      <AboutSection />
-      <ExperienceSection />
-      <ProjectsSection />
-      <TechnologiesSection />
-      <EducationSection />
-      <ContactSection />
-    </main>
+    <>
+      <Head>
+        <title>Łukasz Chlipała - Fullstack Developer Cloud | Portfolio</title>
+        <meta
+          name="description"
+          content="Jestem Fullstack Developerem specjalizującym się w rozwiązaniach chmurowych. Zapraszam do zapoznania się z moim portfolio i doświadczeniem w technologiach takich jak AWS, React, Next.js i TypeScript."
+        />
+        <meta
+          name="keywords"
+          content="Łukasz Chlipała, fullstack developer, chmura, AWS, React, Next.js, TypeScript, portfolio programisty, web development"
+        />
+        <meta name="author" content="Łukasz Chlipała" />
+        <meta name="robots" content="index, follow" />
+      </Head>
+
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <ExperienceSection />
+        <ProjectsSection />
+        <TechnologiesSection />
+        <EducationSection />
+        <ContactSection />
+      </main>
+    </>
   );
 }
